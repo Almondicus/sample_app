15d982f82c08788132887b74f4e6eee07926d5c8
Tim Mandelbaum <tim.mandelbaum@haw-hamburg.de>
2011-06-05 21:09:58 +0200

    Neue App - mit eigener Testroutine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '4a4dfdf99ab76d16a679a4b6b490eef8e4c3ab1d8605815fd078c6f91495a55fe4376aeeea07609eaafae36d3c3fa715fe3a3ae028621b0ef189d613f65cd5ca'
